822dd4de1b98142b299b2d099a22eb9f5ed77b40
alikb <alikb@uw.edu>
2022-06-04 15:35:33 -0700

    create endpoint to get all players in call of duty table that have a higher kd than tag

--- index.js
+++ index.js
@@ -78,6 +78,74 @@ app.get("/callofduty", (req, res) => {
   // });
 });
 
+// endpoint to get all players from the Call of Duty table that have a
+// higher kd than the tag
+app.get(
+  "/callofduty/:tag",
+  (req, res, next) => {
+    const tag = req.params.tag;
+
+    if (!tag) {
+      res.status(400).send({
+        message: "Missing required information.",
+      });
+      return;
+    }
+
+    console.log("passed check tag");
+
+    // check to see that tag exists inside call of duty player
+    const query = "SELECT * FROM Call_Of_Duty WHERE tag=$1";
+    const values = [tag];
+
+    pool
+      .query(query, values)
+      .then((result) => {
+        if (result.rowCount == 0) {
+          res.status(404).send({
+            message: "Tag not found",
+          });
+          return;
+        }
+
+        next();
+      })
+      .catch((err) => {
+        console.log("error: " + err);
+        res.status(400).send({
+          message: "SQL ERROR on checking if tag exists.",
+          error: err,
+        });
+        return;
+      });
+  },
+  (req, res) => {
+    console.log("passed check tag exists in db");
+    // Get the players who have a higher KD than the tag
+    const query = `SELECT * FROM Call_Of_Duty WHERE
+                   kd > (SELECT kd FROM Call_Of_Duty WHERE tag=$1)`;
+
+    const values = [req.params.tag];
+
+    pool
+      .query(query, values)
+      .then((result) => {
+        res.send({
+          success: true,
+          result: result.rows,
+        });
+      })
+      .catch((err) => {
+        console.log("error: " + err);
+        res.status(400).send({
+          message: "SQL ERROR on getting players who have a higher kd than tag",
+          error: err,
+        });
+        return;
+      });
+  }
+);
+
 // endpoint to get all players from the Apex table
 app.get("/apexlegends", (req, res) => {
   const query = "SELECT * FROM Apex_Playoff_Players";
